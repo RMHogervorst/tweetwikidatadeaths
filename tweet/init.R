b7b1e1c3e4237083ea2a2d3dfec69f904a23ba93
Roel Hogervorst <hogervorst.rm@gmail.com>
2018-12-05 18:08:32 +0100

    adds script

---
+++ tweet/init.R
@@ -0,0 +1,8 @@
+my_packages <- c("glue","rtweet","WikidataQueryServiceR")
+
+install_if_missing <- function(p) {
+ if(p %in% rownames(installed.packages())==FALSE){
+install.packages(p)}
+}
+
+invisible(sapply(my_packages, install_if_missing))
